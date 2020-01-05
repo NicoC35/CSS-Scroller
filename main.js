ca4adbb2b6bfa4d69e62c87ec8e6010406eac649
ndqm88 <Nicholas.D.Quintero@gmail.com>
2020-01-04 19:27:00 -0500

    fixed hamburger menu autoclose, as well as added functionality to the up button at the very bottom

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ $(document).ready(function() {
         $('.top-nav').toggleClass('open')
     })
 
-    $('.top_nav .nav-link').on('click', function () {
+    $('.top-nav .nav-link').on('click', function () {
         $(`.menu-toggler`).removeClass('open')
         $('.top-nav').toggleClass('open')
     })
 
     $('nav a[href*="#"]').on('click', function () {
         $('html, body').animate({
-           scrollTop: $($(this).attr('href')).offset().top -100
-        }, 2000);
-    });
-})
+           scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000)
+    })
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+         }, 2000)
+    })
+});
+
